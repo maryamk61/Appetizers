76719336c680068601bc1f845f7c0f25d96f395d
Maryam Kaveh <maryamk61@gmail.com>
2023-09-13 07:04:01 +0330

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Maryam Kaveh on 6/22/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
